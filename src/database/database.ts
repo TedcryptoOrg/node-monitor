a3123a5932a0d28685c67cbe8a2cce493e4b5876
Jose Lopes <josluis.lopes@gmail.com>
2023-08-25 12:23:23 +0100

    chore: lint fix

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,7 +1,7 @@
-import {Model, ModelStatic, Sequelize} from 'sequelize'
-import Monitor from "./models/monitor";
-import Configuration from "./models/configuration";
-import sequelizeConnection from "./config";
+import { type Model, type ModelStatic, type Sequelize } from 'sequelize'
+import Monitor from './models/monitor'
+import Configuration from './models/configuration'
+import sequelizeConnection from './config'
 
 class Database {
   constructor (private readonly sequelize: Sequelize) {
@@ -12,14 +12,14 @@ class Database {
       onUpdate: 'NO ACTION'
     })
     Monitor.belongsTo(Configuration, {
-        foreignKey: 'configuration_id',
-        targetKey: 'id',
-        foreignKeyConstraint: true,
-        constraints: true
+      foreignKey: 'configuration_id',
+      targetKey: 'id',
+      foreignKeyConstraint: true,
+      constraints: true
     })
   }
 
-  model(modelName: string): ModelStatic<Model<any, any>> {
+  model (modelName: string): ModelStatic<Model<any, any>> {
     return this.sequelize.model(modelName)
   }
 
