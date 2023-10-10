0aa66d5e6fb102701c76e6f164f8c64e1b51ba3e
Jose Lopes <josluis.lopes@gmail.com>
2023-10-10 17:19:29 +0100

    chore: lint fix

--- api/src/routes/configurations.ts
+++ api/src/routes/configurations.ts
@@ -1,14 +1,14 @@
-import {create} from "../controllers/configurations/create";
-import {findAll} from "../controllers/configurations/findAll";
-import {update} from "../controllers/configurations/update";
-import {deleteConfiguration} from "../controllers/configurations/delete";
+import { create } from '../controllers/configurations/create'
+import { findAll } from '../controllers/configurations/findAll'
+import { update } from '../controllers/configurations/update'
+import { deleteConfiguration } from '../controllers/configurations/delete'
 
 // Configuration Router
-const configurationRouter = require("express").Router();
+const configurationRouter = require('express').Router()
 
-configurationRouter.post("/", create)
-configurationRouter.get("/", findAll)
-configurationRouter.put("/:id", update)
+configurationRouter.post('/', create)
+configurationRouter.get('/', findAll)
+configurationRouter.put('/:id', update)
 configurationRouter.delete('/:id', deleteConfiguration)
 
-export default configurationRouter;
+export default configurationRouter
