0e5652391591401f5185cf7ce17864bc52539218
Jose Lopes <josluis.lopes@gmail.com>
2023-10-13 22:48:41 +0100

    feat: run migrations when starting the app

---
+++ api/docker/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+# Install sequelize-cli
+bun install sequelize-cli
+
+# Function to check MariaDB readiness
+check_mariadb_ready() {
+  mysql -h $DB_HOST -P $DB_PORT -u $DB_USER -p$DB_PASS -e 'SELECT 1;' > /dev/null 2>&1
+  return $?
+}
+
+# Wait for MariaDB
+until check_mariadb_ready; do
+  echo "Waiting for MariaDB..."
+  sleep 1
+done
+
+# Run migrations
+bunx sequelize-cli db:migrate --url "$DB_DIALECT://$DB_USER:$DB_PASS@$DB_HOST:$DB_PORT/$DB_NAME?version=11.0.3&charset=utf8m4"
+
+# Start the app
+bun start
