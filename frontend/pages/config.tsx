33e18e1a78c5470fc36a1867b07cbe707b68a3e7
Pedro Gonçalves <alfredo@Pedros-MacBook-Pro.local>
2024-03-25 18:11:22 +0000

    config table wip

--- frontend/pages/config.tsx
+++ frontend/pages/config.tsx
@@ -1,12 +1,23 @@
-import { Button, Group, SimpleGrid, Title } from '@mantine/core';
-import TedyTable from '../components/Table';
-import { IconTablePlus } from '@tabler/icons-react';
+import {
+  ActionIcon,
+  Box,
+  Button,
+  Group,
+  SimpleGrid,
+  Title,
+} from '@mantine/core';
+import {
+  IconTablePlus,
+  IconWorldCheck,
+  IconWorldOff,
+} from '@tabler/icons-react';
 import { MantineReactTable, useMantineReactTable } from 'mantine-react-table';
 import { useEffect, useState } from 'react';
 import { RequestType, apiCall } from '../utils/api';
+import { ApiConfiguration } from '../types/ApiConfiguration';
 
 const Configurations = () => {
-  const [configData, setConfigData] = useState(undefined);
+  const [configData, setConfigData] = useState<ApiConfiguration[]>([]);
 
   useEffect(() => {
     getConfigurations();
@@ -28,51 +39,45 @@ const Configurations = () => {
 
   const columns = [
     {
-      accessorKey: 'firstName',
-      header: 'First Name',
+      header: 'ID',
+      accessorKey: 'id',
     },
     {
-      accessorKey: 'lastName',
-      header: 'Last Name',
+      header: 'Name',
+      accessorKey: 'name',
     },
     {
-      accessorKey: 'address',
-      header: 'Address',
+      header: 'Chain',
+      accessorKey: 'chain',
     },
     {
-      accessorKey: 'city',
-      header: 'City',
+      header: 'Enabled',
+      accessorKey: 'is_enabled',
+      mantineTableBodyCellProps: {
+        align: 'center',
+      },
+      Cell: ({ cell }) => (
+        <span>
+          {cell.getValue<boolean>() ? (
+            <IconWorldOff color='#d80e0e' />
+          ) : (
+            <IconWorldCheck color='#1adb00' />
+          )}
+        </span>
+      ),
     },
     {
-      accessorKey: 'state',
       header: 'State',
+      accessorKey: 'state',
     },
   ];
 
   const table = useMantineReactTable({
     columns,
-    configData,
+    data: configData,
     enableRowActions: true,
     renderRowActions: ({ row }) => (
-      <Box style={{ display: 'flex', flexWrap: 'nowrap', gap: '8px' }}>
-        <ActionIcon
-          color='orange'
-          onClick={() => {
-            table.setEditingRow(row);
-          }}
-        >
-          <IconEdit />
-        </ActionIcon>
-        <ActionIcon
-          color='red'
-          onClick={() => {
-            data.splice(row.index, 1); //assuming simple data table
-            setData([...data]);
-          }}
-        >
-          <IconTrash />
-        </ActionIcon>
-      </Box>
+      <Box style={{ display: 'flex', flexWrap: 'nowrap', gap: '8px' }}></Box>
     ),
   });
 
@@ -89,7 +94,7 @@ const Configurations = () => {
       </Group>
 
       <Group justify='center'>
-        <TedyTable table={table}></TedyTable>
+        <MantineReactTable table={table}></MantineReactTable>
       </Group>
     </SimpleGrid>
   );
