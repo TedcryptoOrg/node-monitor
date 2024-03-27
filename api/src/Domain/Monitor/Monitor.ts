b0d104c6752abf26285770bfb8ec10c7d23012c0
Jose Lopes <josluis.lopes@gmail.com>
2024-03-27 14:50:02 +0000

    fix: monitor configuration object not as json

--- api/src/Domain/Monitor/Monitor.ts
+++ api/src/Domain/Monitor/Monitor.ts
@@ -35,6 +35,7 @@ export default class Monitor {
     }
 
     static fromArray(array: MonitorArray): Monitor {
+        console.log('Monitor array', array)
         return new Monitor(
             array.name,
             array.type as MonitorType,
