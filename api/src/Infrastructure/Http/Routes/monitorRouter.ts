da1777cf7f4421c9f06bd46dd465ce954c7b2a61
Jose Lopes <josluis.lopes@gmail.com>
2024-03-30 09:18:29 +0000

    chore: fix test

--- api/src/Infrastructure/Http/Routes/monitorRouter.ts
+++ api/src/Infrastructure/Http/Routes/monitorRouter.ts
@@ -8,13 +8,13 @@ import {findById} from "../../../Ui/Http/Monitors/findById";
 
 const monitorRouter = require('express').Router()
 
+monitorRouter.get('/failed', findFailed)
+monitorRouter.get('/warnings', findWarnings)
 monitorRouter.post('/', upsert)
 monitorRouter.get('/', findAll)
 monitorRouter.get('/:id', findById)
 monitorRouter.put('/:id', upsert)
 monitorRouter.delete('/:id', deleteMonitor)
 monitorRouter.post('/:id/ping', ping)
-monitorRouter.get('/failed', findFailed)
-monitorRouter.get('/warnings', findWarnings)
 
 export default monitorRouter
