ff8ae9f9d6974e4dbb357d5689d0e26d7e023f26
Jose Lopes <josluis.lopes@gmail.com>
2024-03-30 09:20:28 +0000

    chore: lint

--- monitor/src/Domain/NoRecoverableException.ts
+++ monitor/src/Domain/NoRecoverableException.ts
@@ -1,5 +1,5 @@
 export class NoRecoverableException extends Error {
-  constructor (message: string|undefined) {
+  constructor (message: string | undefined) {
     super(message)
     this.name = 'NoRecoverableException'
   }
--- monitor/src/Domain/RecoverableException.ts
+++ monitor/src/Domain/RecoverableException.ts
@@ -1,5 +1,5 @@
 export class RecoverableException extends Error {
-  constructor (message: string|undefined) {
+  constructor (message: string | undefined) {
     super(message)
     this.name = 'recoverableException'
   }
