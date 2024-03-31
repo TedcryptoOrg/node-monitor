4c434b8e4afa6064f0d550675473b3d87b09b679
Pedro Goncalves <9679274+pjgoncalvez@users.noreply.github.com>
2024-03-31 12:24:25 +0100

    adds fix for edit modal in overview configuration overview page (#154)
    Co-authored-by: Pedro Gonçalves <alfredo@Pedros-MacBook-Pro.local>

--- ui/src/components/Chains.tsx
+++ ui/src/components/Chains.tsx
@@ -1,44 +1,45 @@
-import React, {useEffect, useState} from "react";
-import { Autocomplete, TextField } from '@mui/material';
-import {Chain, ChainDirectory} from "@tedcryptoorg/cosmos-directory";
+import React, { useEffect, useState } from "react";
+import { Autocomplete, TextField } from "@mui/material";
+import { Chain, ChainDirectory } from "@tedcryptoorg/cosmos-directory";
 
 const chainDirectory = new ChainDirectory();
 
 interface ChainsAutocompleteProps {
-    chain: string
-    setChain: (chain: string) => void
+  chain: string;
+  setChain: (chain: string) => void;
 }
 
-const ChainsAutocomplete: React.FC<ChainsAutocompleteProps> = (
-    {
-        chain,
-        setChain
-    }) => {
-    const [chains, setChains] = useState<Chain[]>([]);
-    const [loading, setLoading] = useState(true);
+const ChainsAutocomplete: React.FC<ChainsAutocompleteProps> = ({
+  chain,
+  setChain,
+}) => {
+  const [chains, setChains] = useState<Chain[]>([]);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        chainDirectory.getChains().then((response) => {
-            setChains(response.chains);
-            setLoading(false);
-        });
-    }, []);
+  useEffect(() => {
+    chainDirectory.getChains().then((response) => {
+      setChains(response.chains);
+      setLoading(false);
+    });
+  }, []);
 
-    return (
-        <Autocomplete
-            options={chains}
-            loading={loading}
-            selectOnFocus={true}
-            clearOnBlur={true}
-            handleHomeEndKeys={true}
-            getOptionLabel={(option) => option.name}
-            value={chain ? chains.find((c) => c.name === chain) : null}
-            onChange={(event, newValue) => {
-                setChain(newValue ? newValue.name : '');
-            }}
-            renderInput={(params) => <TextField {...params} label="Chain Name" variant="outlined" required />}
-        />
-    );
-}
+  return (
+    <Autocomplete
+      defaultValue={chain}
+      options={chains.map((chain) => chain.name)}
+      loading={loading}
+      selectOnFocus={true}
+      clearOnBlur={true}
+      handleHomeEndKeys={true}
+      getOptionLabel={(option) => option}
+      onChange={(event, newValue) => {
+        setChain(newValue ? newValue : "");
+      }}
+      renderInput={(params) => (
+        <TextField {...params} label="Chain Name" variant="outlined" required />
+      )}
+    />
+  );
+};
 
 export default ChainsAutocomplete;
